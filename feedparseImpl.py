2ea3d00324f50484159b75aabd58ac3d1cf93018
Sachin K S <sachin.kolige@gmail.com>
2014-07-02 16:02:47 +0530

    python 2to3 conversion

--- feedparseImpl.py
+++ feedparseImpl.py
@@ -13,16 +13,16 @@ def printChannelInfo(feedInfo):
     args = ["title","description","link"]
     
     if "url" in feedInfo:
-        print ("\n The url for this feed is: \n %s \n") % feedInfo["url"]
+        print(("\n The url for this feed is: \n %s \n") % feedInfo["url"])
 
     if "version" in feedInfo:
-        print ("The version for this feed is: \n %s \n") % feedInfo["version"]
+        print(("The version for this feed is: \n %s \n") % feedInfo["version"])
 
     if "channel" in feedInfo:
         for arg in args:
             if arg in feedInfo["channel"]:
-                print ("The channel %s  : \n %s ") % (arg,feedInfo["channel"][arg])
-        print "\n"
+                print(("The channel %s  : \n %s ") % (arg,feedInfo["channel"][arg]))
+        print("\n")
 
     
 
@@ -31,15 +31,15 @@ def printFeedItemInfo(feedInfo):
     items = feedInfo["items"]
     args = ["title","summary","link"]
     for item in items:
-        print "########################################"
+        print("########################################")
         for arg in args:
             if arg in item:
-                print ("%s  : \n %s ") % (arg,item[arg])
-        print "\n"
+                print(("%s  : \n %s ") % (arg,item[arg]))
+        print("\n")
     
 
 def main():
-    url = raw_input("enter the RSS feed url to parse: ")
+    url = input("enter the RSS feed url to parse: ")
     feedInfo = feedparser.parse(url)
     # bozo is one of the key/value pair as result of parsing the RSS feed.
     # It is set to 1 if its not a valid RSS feed , else it is set to 0
@@ -47,7 +47,7 @@ def main():
         printChannelInfo(feedInfo)
         printFeedItemInfo(feedInfo)
     else:
-        print "invalid RSS feed URL"
+        print("invalid RSS feed URL")
 
 if __name__=="__main__":
     main()
